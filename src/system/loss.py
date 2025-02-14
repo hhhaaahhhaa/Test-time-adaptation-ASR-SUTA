fix suta-kl bug

src/system/loss.py
@@ -50,7 +50,7 @@ def kl_loss(observations, distribution: Distribution) -> torch.FloatTensor:
     # kde estimation for q distribution with Scott’s Rule
     n = len(observations[0])
     h = n ** (-1. / 5)
-    xs = torch.tensor(distribution.quantiles).reshape(-1, 1)
+    xs = torch.tensor(distribution.quantiles).reshape(-1, 1).to(observations.device)
     log_kde_q = torch.logsumexp(-(xs - observations) ** 2 / 2 / (h ** 2), dim=-1) - math.log(n * h)
     return -log_kde_q.mean()  # since KL(p||q) = E_P(logp - logq), while logp is identical over all choices
         